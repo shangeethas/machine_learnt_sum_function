6296bd1427df69d7f65e7558f9bfcfebfeb727d4
Shangeetha Sivasothy <shangeetha@shangeethas-mbp.dhcp.deakin.edu.au>
2019-06-12 07:57:56 +1000

    Incorporate review comments of Raj and Scott

--- learn.py
+++ learn.py
@@ -5,50 +5,64 @@ from sklearn import linear_model
 import h5py
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense
 import yaml as yaml
 
-with open("configs_learn.yaml", 'r') as stream:
-    try:
-        configs = yaml.safe_load(stream)
-        print(configs)
-    except yaml.YAMLError as exc:
-        print(exc)
 
-directory_name = configs.get('directory_name')
-if not os.path.realpath(directory_name):
-    print('Invalid directory path specified')
-    exit(4)
+def load_train_data():
+    with open("configs_learn.yaml", 'r') as stream:
+        try:
+            configs = yaml.safe_load(stream)
+            print(configs)
+        except yaml.YAMLError as exc:
+            print(exc)
 
-file_name = configs.get('file_name')
-if not os.path.isfile(file_name):
-    print('Invalid file name specified')
-    exit(5)
+    directory_name = configs.get('directory_name')
+    if not os.path.realpath(directory_name):
+        print('Invalid directory path specified')
+        exit(4)
 
-file_str = os.path.join(directory_name, file_name)
-print('Generated file string : ', file_str)
+    file_name = configs.get('file_name')
+    if not os.path.isfile(file_name):
+        print('Invalid file name specified')
+        exit(5)
 
-df = pd.read_csv(file_str)
-print("Training Data Shape : ", df.shape)
-
-d = {'a': df['a'], 'b': df['b'], 'c': df['c'], 'd': df['d']}
-X = pd.DataFrame(data=d)
-
-Y = df['y']
-reg = linear_model.LinearRegression()
-reg.fit(X, Y)
-intercept = reg.intercept_
-coefficients = reg.coef_
+    file_str = os.path.join(directory_name, file_name)
+    print('Generated file string : ', file_str)
+    df = pd.read_csv(file_str)
+    print("Training Data Shape : ", df.shape)
+    return df
 
 
-h5f = h5py.File('linear_reg_model.hdf5', 'w')
-h5f.create_dataset('intercept', data=np.array(intercept))
-h5f.create_dataset('coefficients', data=np.array(coefficients))
-h5f.close()
+def linear_regression_learn(df):
+    d = {'a': df['a'], 'b': df['b'], 'c': df['c'], 'd': df['d']}
+    x = pd.DataFrame(data=d)
 
-model = Sequential()
-model.add(Dense(12, kernel_initializer='uniform',
-                activation='relu', input_shape=X.shape))
-model.compile(optimizer='rmsprop', loss='binary_crossentropy')
-h5f = h5py.File('neural_networks_model.hdf5', 'w')
-h5f.close()
+    y = df['y']
+    reg = linear_model.LinearRegression()
+    reg.fit(x, y)
+    intercept = reg.intercept_
+    coefficients = reg.coef_
+
+    print('Intercept : ', intercept)
+    print('Coefficients : ', coefficients)
+
+    h5f = h5py.File('linear_reg_model.hdf5', 'w')
+    h5f.create_dataset('intercept', data=np.array(intercept))
+    h5f.create_dataset('coefficients', data=np.array(coefficients))
+    h5f.close()
+
+
+def neural_networks_learn():
+    model = Sequential()
+    model.add(Dense(1, kernel_initializer='uniform',
+                    activation='relu', input_shape=X.shape))
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy')
+    model.save('neural_networks_model.hdf5')
+
+
+if __name__ == '__main__':
+    train_df = load_train_data()
+    linear_regression_learn(train_df)
+    # neural_networks_learn()
+
--- predict.py
+++ predict.py
@@ -2,25 +2,31 @@
 import h5py
 import yaml as yaml
 
-with open("configs_predict.yaml", 'r') as stream:
-    try:
-        configs = yaml.safe_load(stream)
-        print(configs)
-    except yaml.YAMLError as exc:
-        print(exc)
 
-a = configs.get('a')
-b = configs.get('b')
-c = configs.get('c')
+def predict():
+    with open("configs_predict.yaml", 'r') as stream:
+        try:
+            configs = yaml.safe_load(stream)
+            print(configs)
+        except yaml.YAMLError as exc:
+            print(exc)
 
-h5f = h5py.File('model.hdf5', 'r')
-intercept = h5f['intercept'][()]
-coefficients = h5f['coefficients'][()]
-h5f.close()
+    a = configs.get('a')
+    b = configs.get('b')
+    c = configs.get('c')
+    d = configs.get('d')
 
-y_actual = a + b + c
-print('Actual Sum Function of {} and {} and {} is : {}'.format(a, b, c, y_actual))
+    h5f = h5py.File('linear_reg_model.hdf5', 'r')
+    intercept = h5f['intercept'][()]
+    coefficients = h5f['coefficients'][()]
+    h5f.close()
 
-y_model = coefficients[0] * a + coefficients[1] * b + coefficients[2] * c
-print('Machine Learnt Sum Function of {} and {} and {} is : {}'.format(a, b, c, y_model))
+    y_actual = a + b + c + d
+    print('Actual Sum Function of {} and {} and {} and {} is : {}'.format(a, b, c, d, y_actual))
 
+    y_model = coefficients[0] * a + coefficients[1] * b + coefficients[2] * c + coefficients[3] * d + intercept
+    print('Machine Learnt Sum Function of {} and {} and {} and {} is : {}'.format(a, b, c, d, y_model))
+
+
+if __name__ == '__main__':
+    predict()
